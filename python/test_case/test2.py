d9c5d1aeddf4c8fb2ed7e90859a0720660d7f32c
Anna Kernozhytskaya <anna.kernozhytskaya@codex-soft.com>
2019-01-04 14:59:13 +0300

    update file test2 and create test3

--- python/test_case/test2.py
+++ python/test_case/test2.py
@@ -9,16 +9,38 @@
 #     print(vowels)
 
 
-phrase = "Don't panic!"
-plist = list(phrase)
-print(phrase)
-print(plist)
-for i in range(4):
-    plist.pop()
-plist.pop(0)
-plist.remove("'")
-plist.extend([plist.pop(), plist.pop()])
-plist.insert(2, plist.pop(3))
-new_phrase = ''.join(plist)
-print(plist)
-print(new_phrase)
+# phrase = "Don't panic!"
+# plist = list(phrase)
+# print(phrase)
+# print(plist)
+# for i in range(4):
+#     plist.pop()
+# plist.pop(0)
+# plist.remove("'")
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
+# new_phrase = ''.join(plist)
+# print(plist)
+# print(new_phrase)
+
+# phrase = "Don't panic!"
+# plist = list(phrase)
+# print(phrase)
+# print(plist)
+# new_phrase = ''.join(plist[1:3])
+# new_phrase = new_phrase + ''.join([plist[5], plist[4], plist [7], plist[6]])
+# print(new_phrase)
+# test = list(new_phrase)
+# print(test)
+
+paranoid_android = "Marvin, the Paranoid Android"
+letters = list(paranoid_android)
+for char in letters[:6]:
+    print('\t', char)
+print()
+for char in letters[-7:]:
+    print('\t'*2, char)
+print()
+for char in letters[12:20]:
+    print('\t' * 3, char)
+print()
