f4e0f7a97e6df274d5d2db751885ba1908e8d99f
Anna Kernozhytskaya <anna.kernozhytskaya@codex-soft.com>
2019-01-06 17:12:09 +0300

    added new functoin in test4

---
+++ python/test_case/test4.py
@@ -0,0 +1,17 @@
+# def search4vowels(word):
+#     """Возвращает гласные, найденные в указанном слове."""
+#     vowels = set('aeiou')
+#     found = vowels.intersection(set(word))
+#     return bool(found)
+
+def search4vowels(phrase: str) -> set:
+    """Возвращает гласные, найденные в указанном слове."""
+    vowels = set('aeiou')
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase: str, letters: str) -> set:
+#def search4letters(phrase: str, letters: str = 'aeiou' ) -> set:
+    """Возвращает множество букв из 'letters', найденныx в указанном слове."""
+    return set(letters).intersection(set(phrase))
+
+# сщодание функций
